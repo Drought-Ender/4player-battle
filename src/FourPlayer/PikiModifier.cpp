4652c3140753e3e6b5b5de7e56bc4c0ae329ff28
Drought-Ender <ethanmucciarone@icloud.com>
2023-06-26 15:32:25 -0500

    remove gamesystem pointers

--- src/FourPlayer/PikiModifier.cpp
+++ src/FourPlayer/PikiModifier.cpp
@@ -9,6 +9,7 @@ namespace Game {
 
 f32 Piki::getAttackDamage()
 {
+	P2ASSERT(this);
 	if (doped()) {
 		return pikiMgr->mParms->mPikiParms.mDopeAttackDamage.mValue;
 	}
@@ -44,18 +45,6 @@ f32 Piki::getThrowHeight()
 		return 0.0f;
 	}
 
-	P2ASSERTLINE(1403, mNavi);
-    if (!gameSystem->isVersusMode()) {
-        switch ((int)mPikiKind) {
-        case Yellow:
-            return static_cast<NaviParms*>(mNavi->mParms)->mNaviParms.mP054.mValue;
-        case Purple:
-            return static_cast<NaviParms*>(mNavi->mParms)->mNaviParms.mQ000.mValue;
-        case White:
-            return static_cast<NaviParms*>(mNavi->mParms)->mNaviParms.mQ001.mValue;
-        }
-    }
-
 	return static_cast<NaviParms*>(mNavi->mParms)->mNaviParms.mP025.mValue;
 }
 
@@ -68,18 +57,6 @@ f32 Piki::getPelletCarryPower()
 {
 	f32 carryPower = 1.0f;
 
-    if (!gameSystem->isVersusMode()) {
-
-        switch ((int)mPikiKind) {
-        case White:
-            carryPower = pikiMgr->mParms->mPikiParms.mWhiteCarryPower.mValue;
-            break;
-        case Purple:
-            carryPower = pikiMgr->mParms->mPikiParms.mPurpleCarryPower.mValue;
-            break;
-        }
-    }
-
 	if (doped() || (int)mHappaKind == Flower) {
 		carryPower += pikiMgr->mParms->mPikiParms.mFlowerCarrySpeedBonus.mValue;
 	} else if ((int)mHappaKind == Bud) {
@@ -91,17 +68,7 @@ f32 Piki::getPelletCarryPower()
 
 f32 Piki::getBaseScale()
 {
-	f32 scale = 1.0f;
 	return 1.0f;
-    if (!gameSystem->isVersusMode()) {
-        if (mPikiKind == Purple) {
-            scale = 1.2f;
-        } else if (mPikiKind == White) {
-            scale = 0.8f;
-        }
-    }
-
-	return scale;
 }
 
 f32 Piki::getSpeed(f32 multiplier)
@@ -121,14 +88,6 @@ f32 Piki::getSpeed(f32 multiplier)
 	int pikiType = mPikiKind;
 	f32 drag     = scaleValue(1.0f, pikiMgr->mParms->mPikiParms.mWalkSpeed.mValue);
 	f32 speed    = multiplier * (baseSpeed - drag) + drag;
-
-    if (!gameSystem->isVersusMode()) {
-        if (pikiType == White) {
-            speed *= pikiMgr->mParms->mPikiParms.mWhiteRunSpeedMultiplier.mValue;
-        } else if (pikiType == Purple) {
-            speed *= pikiMgr->mParms->mPikiParms.mPurpleRunSpeedMultiplier.mValue;
-        }
-    }
 
 	return speed;
 }
@@ -165,10 +124,6 @@ void Piki::setSpeed(f32 multiplier, Vector3f& vec, f32 max)
 int PikiMgr::getColorTransportScale(int color)
 {
 	P2ASSERTBOUNDSLINE(857, 0, color, PikiColorCount);
-	if (color == Purple && !gameSystem->isVersusMode()) {
-		return 10;
-	}
-
 	return 1;
 }
 
@@ -178,7 +133,7 @@ int Piki::getDownfloorMass()
 		return 0;
 	}
 
-	return ((int)mPikiKind == Purple && !gameSystem->isVersusMode()) ? 10 : 1;
+	return 1;
 }
 
 }
