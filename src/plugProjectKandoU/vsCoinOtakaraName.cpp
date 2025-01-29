63f73cd5946bc741348f591beb0169b9af9d5412
Drought-Ender <87912640+Drought-Ender@users.noreply.github.com>
2025-01-28 23:19:19 -0600

    Update vsCoinOtakaraName.cpp

--- src/plugProjectKandoU/vsCoinOtakaraName.cpp
+++ src/plugProjectKandoU/vsCoinOtakaraName.cpp
@@ -5,11 +5,10 @@ const char* VsOtakaraName::cCoin         = "flower_red";       // slot machine c
 const char* VsOtakaraName::cBedamaBlue   = "be_dama_blue_l";   // blue marble
 const char* VsOtakaraName::cBedamaRed    = "be_dama_red_l";    // red marble
 const char* VsOtakaraName::cBedamaYellow = "be_dama_yellow_l"; // yellow marble
-const char* VsOtakaraName::cBedamaPurple = "be_dama_purple_l";
-const char* VsOtakaraName::cBedamaWhite  = "be_dama_white_l";
+const char* VsOtakaraName::cBedamaPurple = "be_dama_purple_l"; // purple marble
+const char* VsOtakaraName::cBedamaWhite  = "be_dama_white_l";  // white marble
 const char* VsOtakaraName::cBedamaMini   = "be_dama_yellow";   // small yellow marble
-const char* VsOtakaraName::cBingoRandom  = "g_futa_hiruzen"; // president head
+const char* VsOtakaraName::cBingoRandom  = "be_dama_lucky";    // president head
 float VsOtakaraName::cBedamaYellowDepth  = 20.0f;              // bury depth of yellow marble
 float VsOtakaraName::cBedamaRedBlueDepth = 20.0f;              // bury depth of red/blue marbles
 float VsOtakaraName::cBedamaMiniDepth    = 5.0f;               // bury depth of small yellow marble
-
